eca8e7931a2c6332570100a7707974fca3b2debb
Luiz Eduardo <luiz.lr.2010@gmail.com>
2021-07-12 16:52:20 -0300

    Estilizando os componentes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import GlobalStyle from './styles/global';
+import Header from './components/Header';
+import Board from './components/Board';
 
 function App() {
   return (
-    <div className="App">
-     <GlobalStyle></GlobalStyle>
-    </div>
+    <>
+      <Header />
+      <Board />
+      
+      <GlobalStyle />
+    </>        
   );
 }
 
---
+++ src/components/Board/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Container } from './styles';
+import List from '../List';
+
+export default function Board(){
+  return(
+  <Container>
+    <List />
+    <List />
+    <List />
+    <List />
+  </Container>
+  );
+}
