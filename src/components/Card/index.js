f447036fadeacf0625e131af63eb988f68331d73
Luizrtr <luiz.lr.2010@gmail.com>
2021-07-12 20:34:28 -0300

    Adicionando uma list em um arquivo api

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import logo from '../../logo.png';
 import { Container, Label } from './styles';
 
-export default function Card(){
+export default function Card({ data }){
   return(
   <Container>
     <header>
-      <Label color="#7159c1" />
+      {data.labels.map(label => <Label key={label} color={label} />)}      
     </header>
-    <p>Fazer a migração completa de servidor</p>
-    <img src={logo} alt="logo" />
+    <p>{data.content}</p>
+    { data.user && <img src={data.user} alt="" />}
   </Container>
   );
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -2,22 +2,21 @@ import React from 'react';
 import { MdAdd } from 'react-icons/md';
 import Card from '../Card';
 import { Container } from './styles';
-export default function List(){
+
+export default function List({ data }){
   return(
   <Container>
     <header>
-      <h2>Tarefas</h2>
-      <button type="button">
-        <MdAdd size={24} color="#FFF" />
-      </button>
+      <h2>{data.title}</h2>
+      {data.creatable && (
+        <button type="button">
+          <MdAdd size={24} color="#FFF" />
+        </button> 
+      )}
     </header>
 
     <ul>
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
+      { data.cards.map(card => <Card key={card.id} data={card} />) }
     </ul>
   </Container>
   );
