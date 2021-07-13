afa6a6df3be5eb023fd10eb8e01b318c80a7ebb8
Luizrtr <luiz.lr.2010@gmail.com>
2021-07-12 21:05:11 -0300

    useDrag implementado

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
-import logo from '../../logo.png';
+import { useDrag } from 'react-dnd';
+
 import { Container, Label } from './styles';
 
+
 export default function Card({ data }){
+  const [{ isDragging } , dragRef] = useDrag({ //dragRef coloca no elemento que seja arrastável
+    type: 'CARD', // Todo item arrastável precisa ter um tipo
+    collect: monitor => ({ //Coletar informações sobre o Drag.
+      isDragging: monitor.isDragging(),
+    }),
+  }); 
   return(
-  <Container>
+  <Container ref={dragRef} isDragging={isDragging}>
     <header>
       {data.labels.map(label => <Label key={label} color={label} />)}      
     </header>
